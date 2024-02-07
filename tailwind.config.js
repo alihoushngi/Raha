a5fda56fd1da26673ff746e15e4d95bd8d3beccf
Ali Houshangi <ali@Alis-MacBook-Air.local>
2024-02-07 11:27:22 +0330

    header done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#BEA76B",
+        secondary: "#BEBEBE",
+      },
+    },
   },
   plugins: [],
 };
